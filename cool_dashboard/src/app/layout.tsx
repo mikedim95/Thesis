d3fd4a1a49f3d020a08038b0e8dfb54e34b06299
mikedim95 <mikedim95@gmail.com>
2024-06-14 18:02:35 +0300

    import analytics

--- cool_dashboard/src/app/layout.tsx
+++ cool_dashboard/src/app/layout.tsx
@@ -7,6 +7,7 @@ import React, { useEffect, useState } from "react";
 import Loader from "@/components/common/Loader";
 import AuthProvider from "./context/AuthProvider";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -26,6 +27,7 @@ export default function RootLayout({
       <body suppressHydrationWarning={true}>
         <AuthProvider>
           <SpeedInsights />
+          <Analytics />
           <div className="dark:bg-boxdark-2 dark:text-bodydark">
             {loading ? <Loader /> : children}
           </div>
