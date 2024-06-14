7ac1e254ca8b48dbf6f0df8a6105f5c0e45796bd
mikedim95 <mikedim95@gmail.com>
2024-06-14 18:38:20 +0300

    no middleware for checking

--- cool_dashboard/src/middleware.ts
+++ cool_dashboard/src/middleware.ts
@@ -1,4 +1,4 @@
-export { default } from "next-auth/middleware";
+/* export { default } from "next-auth/middleware";
 
 export const config = {
   matcher: [
@@ -15,3 +15,4 @@ export const config = {
     signIn: "/auth/login",
   },
 };
+ */
